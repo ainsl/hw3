67c3d3c63900f41646b7a674a7704d44b4ab42ba
ainsl <60450465+ainsl@users.noreply.github.com>
2020-02-12 13:46:38 -0800

    Create height-lawnmower.js

---
+++ height-lawnmower.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB);
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x+random(-10, 10), height-10-random(h));
+  noStroke();
+
+  x = x + 10;
+
+  if (x > width) {
+    x = random(10);
+    h = h + 3;
+  }
+
+  // the variable h represents the height of the grass
+  // to change the when the lawnmower comes, modify the if statement
+  // (h > 50) means to cut the grass when the height is over 50
+  if (h > 50) {
+    fill(255);
+    rect(0, 0, width, height-15);
+    h = 10;
+  }
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
